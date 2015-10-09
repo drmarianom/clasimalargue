66bf897badacc3a88ac7b904a31a83423a4bff02
mariano <drmarianom@gmail.com>
2015-10-09 16:44:12 -0300

    Last update on website and all the documentation on documents directory;

--- frontend/main.php
+++ frontend/main.php
@@ -8,33 +8,179 @@
     <?php require_once("header.php"); ?>
     <!-- header end -->
     <!-- body begin -->
-    <div style="border:1px solid silver; margin:30px 100px 30px 100px; text-align:center;">
-      <div>Publicaciones recientes</div>
-      <table border="0" width="100%">
+
+    <table border="0" width="100%">
+      <tr>
+    <td width="200px" valign="top" align="center">
+      <h4>Publicidades</h4>
+      <img src="../images/banner.png" />
+      <img src="../images/banner.png" />
+      <img src="../images/banner.png" />
+      <img src="../images/banner.png" />
+    </td>
+    <td align="center">
+      <h4>Publicaciones recientes</h4>
+      <table border="0" align="center">
         <tr>
-      <td><a href="#"><img src="../images/ileft.png" /></a></td>
       <td><a href="detail.php"><img src="../images/publicaciones/producto-size-1.jpg" width="100px" /><div>producto</div><div>$100</div></a></td>
       <td><a href="detail.php"><img src="../images/publicaciones/producto-size-1.jpg" width="100px" /><div>producto</div><div>$100</div></a></td>
       <td><a href="detail.php"><img src="../images/publicaciones/producto-size-1.jpg" width="100px" /><div>producto</div><div>$100</div></a></td>
       <td><a href="detail.php"><img src="../images/publicaciones/producto-size-1.jpg" width="100px" /><div>producto</div><div>$100</div></a></td>
       <td><a href="detail.php"><img src="../images/publicaciones/producto-size-1.jpg" width="100px" /><div>producto</div><div>$100</div></a></td>
       <td><a href="detail.php"><img src="../images/publicaciones/producto-size-1.jpg" width="100px" /><div>producto</div><div>$100</div></a></td>
-      <td><a href="#"><img src="../images/iright.png" /></a></td>
+        </tr>
+        <tr>
+      <td><a href="detail.php"><img src="../images/publicaciones/producto-size-1.jpg" width="100px" /><div>producto</div><div>$100</div></a></td>
+      <td><a href="detail.php"><img src="../images/publicaciones/producto-size-1.jpg" width="100px" /><div>producto</div><div>$100</div></a></td>
+      <td><a href="detail.php"><img src="../images/publicaciones/producto-size-1.jpg" width="100px" /><div>producto</div><div>$100</div></a></td>
+      <td><a href="detail.php"><img src="../images/publicaciones/producto-size-1.jpg" width="100px" /><div>producto</div><div>$100</div></a></td>
+      <td><a href="detail.php"><img src="../images/publicaciones/producto-size-1.jpg" width="100px" /><div>producto</div><div>$100</div></a></td>
+      <td><a href="detail.php"><img src="../images/publicaciones/producto-size-1.jpg" width="100px" /><div>producto</div><div>$100</div></a></td>
         </tr>
       </table>
-    </div>
-    <div style="border:1px solid silver; margin:30px 100px 30px 100px; text-align:center;">
-      <div>Principales categrorías</div>
-      Categorías<br/>
-      Categorías<br/>
-      Categorías<br/>
-      Categorías<br/>
-      Categorías<br/>
-      Categorías<br/>
-      Categorías<br/>
-      Categorías<br/>
-      Categorías<br/>
-    </div>
+      
+      <h4>Categorías</h4>
+      <div>
+
+      
+      
+      
+        Categorías<br/>
+	Categorías<br/>
+	Categorías<br/>
+	Categorías<br/>
+	Categorías<br/>
+	Categorías<br/>
+	
+	
+articulos (compra / venta)
+
+  artículos del hogar
+  computación + tecnología
+  fotografía + audio + video
+  consolas + video juegos
+  celulares
+  bicicletas
+  deporte + tiempo libre
+  ropa + accesorios
+  accesorios niños y bebés
+  joyas + relojes
+  juguetes
+  instrumentos musicales
+  antigüedades
+  muebles
+  libros + revistas
+  vino + gastronomía
+  montaña + ski
+  electrónica
+  salud + belleza
+  herramientas
+    
+empleos (pedidos / ofrecidos)
+
+  informática + internet
+  ingenieros + arquitectos
+  medicina + salud
+  diseño + creatividad
+  márketing + comunicación
+  comerciales + ventas
+  Telefonístas
+  secretarias + recepcionistas
+  contabilidad + finanzas
+  educacionales
+  hotelería + restaurantes
+  peluquería + estética
+  prácticas + pasantías
+  trabajos temporales
+  administración
+  Contratistas
+  Cuidadores
+  Niñeras
+  Empleadas Domésticas
+
+Servicios para la comunidad
+
+  mascotas + animales
+  general
+  cumpleaños
+  intercambio de idiomas
+  trueques
+  artistas + músicos
+  cuidado de personas + niñera
+  objetos perdidos
+  Animales Perdidos
+  compartir auto
+
+vehículos (compra / venta / alquiler)
+
+  autos
+  vehículos 4x4
+  motos + cuatriciclos
+  partes + accesorios
+  camiones
+  camionetas
+  autos de colección
+  alquiler de vehículos
+    
+servicios 
+
+  informática + diseño web
+  imagen + sonido + diseño
+  gráfico
+  editorial + traducciones
+  gestoría + legal
+  eventos
+  estética + moda + terapias + masajes
+  limpieza + servicio doméstico
+  transporte + mudanza
+  tarot + astrología
+  socios para negocios
+  viajes + turismo
+  hoteles + hostels
+  hotel alojamiento
+  pymes
+  Masajistas
+  Aticúlos Regionales
+  otros
+    
+cursos/clases 
+
+  idiomas
+  música + teatro + danza
+  informática
+  educación
+  deportes
+  Artes Marciales
+
+inmuebles
+
+  departamentos en alquiler
+  casas en alquiler
+  departamentos en venta
+  casas en venta
+  lotes
+  locales comerciales
+  oficinas
+  habitaciones + compartidos
+  alquileres de vacaciones
+  Cabañas
+  Hoteles
+
+	
+	
+      </div>
+      
+    </td>
+    <td width="200px" valign="top" align="center">
+      <h4>Componentes</h4>
+      <img src="../images/componente.png" />
+      <img src="../images/componente.png" />
+      <img src="../images/componente.png" />
+      <img src="../images/componente.png" />
+    </td>
+      </tr>
+    </table>
+    
     <!-- body end -->
     <!-- foot begin -->
     <?php require_once("footer.php"); ?>
